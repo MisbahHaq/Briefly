627acd6bc62dfe71b87fca01acc359611f02bb2b
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-11 10:56:54 +0500

    Update

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(244, 255, 255, 255),
+      backgroundColor: const Color.fromARGB(255, 243, 243, 243),
       body: Container(
         margin: EdgeInsets.only(top: 40, left: 25),
         child: Column(
@@ -43,22 +43,61 @@ class _HomeState extends State<Home> {
               "Hottest News",
               style: TextStyle(
                   color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 10,
             ),
             Row(
               children: [
-                Container(
-                  decoration: BoxDecoration(color: Colors.white),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        "assets/images/news1.jpg",
-                        width: MediaQuery.of(context).size.width / 2.1,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
-                    ],
+                Material(
+                  elevation: 2,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
+                            "assets/images/news1.jpg",
+                            width: MediaQuery.of(context).size.width / 1.8,
+                            height: 150,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 1.8,
+                          child: Text(
+                            "Hottest News",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 1.8,
+                          child: Text(
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                            style: TextStyle(
+                                color: const Color.fromARGB(151, 0, 0, 0),
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 )
               ],
