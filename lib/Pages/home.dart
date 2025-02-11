3ecc8ec9491d815a3b39262f183fbbd8a878f75b
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-11 11:23:08 +0500

    Update

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -55,19 +55,21 @@ class _HomeState extends State<Home> {
                   elevation: 2,
                   borderRadius: BorderRadius.circular(10),
                   child: Container(
-                    padding: EdgeInsets.all(10),
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(10)),
                     child: Column(
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset(
-                            "assets/images/news1.jpg",
-                            width: MediaQuery.of(context).size.width / 1.8,
-                            height: 150,
-                            fit: BoxFit.cover,
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              "assets/images/news1.jpg",
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              height: 150,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                         SizedBox(
@@ -76,6 +78,7 @@ class _HomeState extends State<Home> {
                         Container(
                           width: MediaQuery.of(context).size.width / 1.8,
                           child: Text(
+                            textAlign: TextAlign.center,
                             "Hottest News",
                             style: TextStyle(
                                 color: Colors.black,
@@ -87,8 +90,10 @@ class _HomeState extends State<Home> {
                           height: 5,
                         ),
                         Container(
+                          padding: EdgeInsets.only(left: 5, right: 5),
                           width: MediaQuery.of(context).size.width / 1.8,
                           child: Text(
+                            textAlign: TextAlign.center,
                             "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
                             style: TextStyle(
                                 color: const Color.fromARGB(151, 0, 0, 0),
@@ -96,6 +101,25 @@ class _HomeState extends State<Home> {
                                 fontWeight: FontWeight.bold),
                           ),
                         ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Color(0xff3280ef),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                            ),
+                          ),
+                          margin: EdgeInsets.only(left: 160),
+                          child: Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        )
                       ],
                     ),
                   ),
