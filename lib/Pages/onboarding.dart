3a9cd4320862fad1c572fd52231cb0c19a95644c
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-11 10:13:28 +0500

    Update

--- lib/Pages/onboarding.dart
+++ lib/Pages/onboarding.dart
@@ -11,24 +11,79 @@ class _OnBoardingState extends State<OnBoarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Image.asset(
-              "assets/images/onboard.jpg",
-              height: MediaQuery.of(context).size.height / 1.7,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height / 1.7),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: Stack(
+            children: [
+              Image.asset(
+                "assets/images/onboard.jpg",
+                height: MediaQuery.of(context).size.height / 1.7,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
               ),
-            )
-          ],
+              Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 1.8),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      "Get The Latest And Updated\nNews, Briefly.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Image.asset(
+                      "assets/images/newspaper.png",
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Container(
+                      height: 60,
+                      margin: EdgeInsets.only(left: 40, right: 40),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Color(0xff3280ef),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Get Started",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
