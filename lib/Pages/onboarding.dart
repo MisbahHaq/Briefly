a5a161c7005f15e95f52f38c0b05903fcd66f254
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-11 09:49:35 +0500

    Update

--- lib/Pages/onboarding.dart
+++ lib/Pages/onboarding.dart
@@ -10,6 +10,27 @@ class OnBoarding extends StatefulWidget {
 class _OnBoardingState extends State<OnBoarding> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/onboard.jpg",
+              height: MediaQuery.of(context).size.height / 1.7,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 1.7),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
