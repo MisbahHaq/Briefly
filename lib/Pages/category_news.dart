f17ec3c2311b7b86e13731a5f7a5526719dd8130
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-13 10:45:46 +0500

    Update

--- lib/Pages/category_news.dart
+++ lib/Pages/category_news.dart
@@ -1,13 +1,28 @@
+import 'package:briefly/Services/show_category_news.dart';
 import 'package:flutter/material.dart';
 
 class CategoryNews extends StatefulWidget {
-  const CategoryNews({super.key});
+  String name;
+  CategoryNews({required this.name});
 
   @override
   State<CategoryNews> createState() => _CategoryNewsState();
 }
 
 class _CategoryNewsState extends State<CategoryNews> {
+  List<ShowCategoryNews> categories = [];
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  getNews() async {
+    ShowCategoryNews showCategoryNews = ShowCategoryNews();
+    await showCategoryNews.getCategoryNews(widget.name.toLowerCase());
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
