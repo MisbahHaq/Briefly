3288c27a3e4d563961b1716481ce37f67f5dffde
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-13 10:01:23 +0500

    API Received

--- lib/Services/news.dart
+++ lib/Services/news.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:briefly/Models/article_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -10,21 +9,29 @@ class News {
   Future<void> getNews() async {
     String url =
         "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=f330ef1f67634cec871eebf7ea97b79d";
-    var response = await http.get(Uri.parse(url));
+    try {
+      var response = await http.get(Uri.parse(url));
 
-    var jsonData = jsonDecode(response.body);
-
-    if (jsonData['status'] == 'ok') {
-      jsonData["articles"].forEach((element) {
-        if (element["urlToImage"] != null && element["decription"] != null) {
-          ArticleModel articleModel = ArticleModel(
-            urlToImage: element["urlToImage"],
-            desc: element["description"],
-            title: element["title"],
-          );
-          news.add(articleModel);
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(response.body);
+        if (jsonData['status'] == 'ok') {
+          jsonData["articles"].forEach((element) {
+            if (element["urlToImage"] != null &&
+                element["description"] != null) {
+              ArticleModel articleModel = ArticleModel(
+                urlToImage: element["urlToImage"],
+                desc: element["description"],
+                title: element["title"],
+              );
+              news.add(articleModel);
+            }
+          });
         }
-      });
+      } else {
+        print("Failed to load data: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error fetching news: $e");
     }
   }
 }
